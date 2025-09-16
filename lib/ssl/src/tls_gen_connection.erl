hanssv/otp

commit 28f7e80233718f9f42fe8863370f7e99ee14d22c

Move max_fragment_size
Move it from connection_state to connection_stats map and make it
optional.

Slight memory saving.

lib/ssl/src/tls_gen_connection.erl
@@ -658,7 +658,7 @@ next_tls_record(Data, StateName,
             _ ->
                 State0#state.connection_env#connection_env.negotiated_version
         end,
-    #{current_write := #{max_fragment_length := MaxFragLen}} = State0#state.connection_states,
+    MaxFragLen = maps:get(max_fragment_length, State0#state.connection_states, undefined),
     SslOpts = maps:put(downgrade, Downgrade, SslOpts0),
     case tls_record:get_tls_records(Data, Versions, Buf0, MaxFragLen, SslOpts) of
 	{Records, Buf1} ->

lib/ssl/src/dtls_gen_connection.erl
@@ -320,7 +320,7 @@ send_handshake_flight(#state{static_env = #static_env{socket = Socket,
 			     connection_states = ConnectionStates0,
                              ssl_options = #{log_level := LogLevel}} = State0,
                       Epoch) ->
-    #{current_write := #{max_fragment_length := MaxFragmentLength}} = ConnectionStates0,
+    MaxFragmentLength = maps:get(max_fragment_length, ConnectionStates0, undefined),
     MaxSize = mtu(MaxFragmentLength, DataTag),
     {Encoded, ConnectionStates} =
 	encode_handshake_flight(lists:reverse(Flight), Version, MaxSize, Epoch, ConnectionStates0),
@@ -338,7 +338,7 @@ send_handshake_flight(#state{static_env = #static_env{socket = Socket,
 			     connection_states = ConnectionStates0,
                              ssl_options = #{log_level := LogLevel}} = State0,
                       Epoch) ->
-    #{current_write := #{max_fragment_length := MaxFragmentLength}} = ConnectionStates0,
+    MaxFragmentLength = maps:get(max_fragment_length, ConnectionStates0, undefined),
     MaxSize = mtu(MaxFragmentLength, DataTag),
     {HsBefore, ConnectionStates1} =
 	encode_handshake_flight(lists:reverse(Flight0), Version, MaxSize, Epoch, ConnectionStates0),
@@ -358,7 +358,7 @@ send_handshake_flight(#state{static_env = #static_env{socket = Socket,
 			     connection_states = ConnectionStates0,
                              ssl_options = #{log_level := LogLevel}} = State0,
                       Epoch) ->
-    #{current_write := #{max_fragment_length := MaxFragmentLength}} = ConnectionStates0,
+    MaxFragmentLength = maps:get(max_fragment_length, ConnectionStates0, undefined),
     MaxSize = mtu(MaxFragmentLength, DataTag),
     {HsBefore, ConnectionStates1} =
 	encode_handshake_flight(lists:reverse(Flight0), Version, MaxSize, Epoch-1, ConnectionStates0),
@@ -382,7 +382,7 @@ send_handshake_flight(#state{static_env = #static_env{socket = Socket,
 			     connection_states = ConnectionStates0,
                              ssl_options = #{log_level := LogLevel}} = State0,
                       Epoch) ->
-    #{current_write := #{max_fragment_length := MaxFragmentLength}} = ConnectionStates0,
+    MaxFragmentLength = maps:get(max_fragment_length, ConnectionStates0, undefined),
     MaxSize = mtu(MaxFragmentLength, DataTag),
     {EncChangeCipher, ConnectionStates1} = 
 	encode_change_cipher(ChangeCipher, Version, Epoch-1, ConnectionStates0),

lib/ssl/src/dtls_record.erl
@@ -223,13 +223,8 @@ encode_change_cipher_spec(Version, Epoch, ConnectionStates) ->
 %% Description: Encodes data to send on the ssl-socket.
 %%--------------------------------------------------------------------
 encode_data(Data, Version, ConnectionStates) ->
-    #{epoch := Epoch, max_fragment_length := MaxFragmentLength}
-        = ssl_record:current_connection_state(ConnectionStates, write),
-    MaxLength = if is_integer(MaxFragmentLength) ->
-                        MaxFragmentLength;
-                   true ->
-                        ?MAX_PLAIN_TEXT_LENGTH
-                end,
+    #{epoch := Epoch} = ssl_record:current_connection_state(ConnectionStates, write),
+    MaxLength = maps:get(max_fragment_length, ConnectionStates, ?MAX_PLAIN_TEXT_LENGTH),
     case iolist_size(Data) of
 	N when N > MaxLength ->
             Frags = tls_record:split_iovec(erlang:iolist_to_iovec(Data), MaxLength),
@@ -417,8 +412,7 @@ initial_connection_state(ConnectionEnd, BeastMitigation) ->
       mac_secret  => undefined,
       secure_renegotiation => undefined,
       client_verify_data => undefined,
-      server_verify_data => undefined,
-      max_fragment_length => undefined
+      server_verify_data => undefined
      }.
 
 get_dtls_records_aux({DataTag, StateName, _, Versions} = Vinfo,

lib/ssl/src/ssl_gen_statem.erl
@@ -1840,7 +1840,7 @@ connection_info(#state{handshake_env = #handshake_env{sni_hostname = SNIHostname
                                           cipher_suite = CipherSuite,
                                           srp_username = SrpUsername,
                                           ecc = ECCCurve} = Session,
-                       connection_states = #{current_write := CurrentWrite},
+                       connection_states = ConnectionStates,
 		       connection_env = #connection_env{negotiated_version =  {_,_} = Version},
 		       ssl_options = #{protocol := Protocol} = Opts}) ->
     RecordCB = record_cb(Protocol),
@@ -1853,7 +1853,7 @@ connection_info(#state{handshake_env = #handshake_env{sni_hostname = SNIHostname
 		    _ ->
 			[]
 		end,
-    MFLInfo = case maps:get(max_fragment_length, CurrentWrite, undefined) of
+    MFLInfo = case maps:get(max_fragment_length, ConnectionStates, undefined) of
                   MaxFragmentLength when is_integer(MaxFragmentLength) ->
                       [{max_fragment_length, MaxFragmentLength}];
                   _ ->

lib/ssl/src/ssl_handshake.erl
@@ -1505,7 +1505,7 @@ handle_server_hello_extensions(RecordCB, Random, CipherSuite,
     %% RFC 6066: handle received/expected maximum fragment length
     if IsNew ->
             ServerMaxFragEnum = maps:get(max_frag_enum, Exts, undefined),
-            #{current_write := #{max_fragment_length := ConnMaxFragLen}} = ConnectionStates,
+            ConnMaxFragLen = maps:get(max_fragment_length, ConnectionStates0, undefined),
             ClientMaxFragEnum = max_frag_enum(ConnMaxFragLen),
 
             if ServerMaxFragEnum == ClientMaxFragEnum ->

lib/ssl/src/ssl_record.erl
@@ -222,29 +222,14 @@ set_renegotiation_flag(Flag, #{current_read := CurrentRead0,
 %%
 %% Description: Set maximum fragment length in all connection states
 %%--------------------------------------------------------------------
-set_max_fragment_length(#max_frag_enum{enum = MaxFragEnum},
-                        #{current_read := CurrentRead0,
-                          current_write := CurrentWrite0,
-                          pending_read := PendingRead0,
-                          pending_write := PendingWrite0}
-                        = ConnectionStates) when (MaxFragEnum == 1) orelse
-                                                 (MaxFragEnum == 2) orelse
-                                                 (MaxFragEnum == 3) orelse
-                                                 (MaxFragEnum == 4)
-                                                 ->
+set_max_fragment_length(#max_frag_enum{enum = MaxFragEnum}, ConnectionStates)
+  when is_integer(MaxFragEnum), 1 =< MaxFragEnum, MaxFragEnum =< 4 ->
     MaxFragmentLength = if MaxFragEnum == 1 -> ?MAX_FRAGMENT_LENGTH_BYTES_1;
                            MaxFragEnum == 2 -> ?MAX_FRAGMENT_LENGTH_BYTES_2;
                            MaxFragEnum == 3 -> ?MAX_FRAGMENT_LENGTH_BYTES_3;
                            MaxFragEnum == 4 -> ?MAX_FRAGMENT_LENGTH_BYTES_4
                         end,
-    CurrentRead = CurrentRead0#{max_fragment_length => MaxFragmentLength},
-    CurrentWrite = CurrentWrite0#{max_fragment_length => MaxFragmentLength},
-    PendingRead = PendingRead0#{max_fragment_length => MaxFragmentLength},
-    PendingWrite = PendingWrite0#{max_fragment_length => MaxFragmentLength},
-    ConnectionStates#{current_read => CurrentRead,
-		      current_write => CurrentWrite,
-		      pending_read => PendingRead,
-		      pending_write => PendingWrite};
+    ConnectionStates#{max_fragment_length => MaxFragmentLength};
 set_max_fragment_length(_,ConnectionStates) ->
     ConnectionStates.
 
@@ -462,7 +447,6 @@ empty_connection_state(ConnectionEnd, Version,
       client_verify_data => undefined,
       server_verify_data => undefined,
       pending_early_data_size => MaxEarlyDataSize,
-      max_fragment_length => undefined,
       trial_decryption => false,
       early_data_accepted => false
      }.

lib/ssl/src/tls_record.erl
@@ -108,10 +108,10 @@ init_connection_states(Role, Version, BeastMitigation, MaxEarlyDataSize) ->
         Buffer0 :: binary() | {'undefined' | #ssl_tls{}, {[binary()],non_neg_integer(),[binary()]}},
         tls_max_frag_len(),
         ssl_options()) ->
-                             {Records :: [#ssl_tls{}],
-                              Buffer :: {'undefined' | #ssl_tls{}, {[binary()],non_neg_integer(),[binary()]}}} |
-                             #alert{}.
-%%			     
+          {Records :: [#ssl_tls{}],
+           Buffer :: {'undefined' | #ssl_tls{}, {[binary()],non_neg_integer(),[binary()]}}} |
+          #alert{}.
+%%
 %% and returns it as a list of binaries also returns leftover
 %% Description: Given old buffer and new data from TCP, packs up a records
 %% data
@@ -133,18 +133,13 @@ get_tls_records(Data, Versions, {Hdr, {Front,Size,Rear}}, MaxFragLen, SslOpts) -
 %%--------------------------------------------------------------------
 encode_handshake(Frag, ?TLS_1_3, ConnectionStates) ->
     tls_record_1_3:encode_handshake(Frag, ConnectionStates);
-encode_handshake(Frag, Version, 
+encode_handshake(Frag, Version,
 		 #{current_write :=
 		       #{beast_mitigation := BeastMitigation,
-                         max_fragment_length := MaxFragmentLength,
-			  security_parameters :=
-			     #security_parameters{bulk_cipher_algorithm = BCA}}} = 
+                         security_parameters :=
+			     #security_parameters{bulk_cipher_algorithm = BCA}}} =
 		     ConnectionStates) ->
-    MaxLength = if is_integer(MaxFragmentLength) ->
-                        MaxFragmentLength;
-                   true ->
-                        ?MAX_PLAIN_TEXT_LENGTH
-                end,
+    MaxLength = maps:get(max_fragment_length, ConnectionStates, ?MAX_PLAIN_TEXT_LENGTH),
     case iolist_size(Frag) of
 	N when N > MaxLength ->
             Data = split_iovec(erlang:iolist_to_iovec(Frag), Version, BCA, BeastMitigation, MaxLength),
@@ -184,16 +179,12 @@ encode_change_cipher_spec(Version, ConnectionStates) ->
 encode_data(Data, ?TLS_1_3, ConnectionStates) ->
     tls_record_1_3:encode_data(Data, ConnectionStates);
 encode_data(Data, Version,
-	    #{current_write := #{beast_mitigation := BeastMitigation,
-                                 max_fragment_length := MaxFragmentLength,
-				 security_parameters :=
-				     #security_parameters{bulk_cipher_algorithm = BCA}}} =
+	    #{current_write :=
+                  #{beast_mitigation := BeastMitigation,
+                    security_parameters :=
+                        #security_parameters{bulk_cipher_algorithm = BCA}}} =
 		ConnectionStates) ->
-    MaxLength = if is_integer(MaxFragmentLength) ->
-                        MaxFragmentLength;
-                   true ->
-                        ?MAX_PLAIN_TEXT_LENGTH
-                end,
+    MaxLength = maps:get(max_fragment_length, ConnectionStates, ?MAX_PLAIN_TEXT_LENGTH),
     Fragments = split_iovec(Data, Version, BCA, BeastMitigation, MaxLength),
     encode_fragments(?APPLICATION_DATA, Version, Fragments, ConnectionStates).
 
@@ -479,7 +470,6 @@ initial_connection_state(ConnectionEnd, BeastMitigation, MaxEarlyDataSize) ->
       client_verify_data => undefined,
       server_verify_data => undefined,
       pending_early_data_size => MaxEarlyDataSize,
-      max_fragment_length => undefined,
       trial_decryption => false,
       early_data_expected => false
      }.

lib/ssl/src/tls_record_1_3.erl
@@ -46,15 +46,8 @@
 %
 %% Description: Encodes a handshake message to send on the tls-1.3-socket.
 %%--------------------------------------------------------------------
-encode_handshake(Frag, #{current_write :=
-                             #{max_fragment_length := MaxFragmentLength}} =
-                     ConnectionStates) ->
-    MaxLength = if is_integer(MaxFragmentLength) ->
-                        MaxFragmentLength;
-                   true ->
-                        %% TODO: Consider padding here
-                        ?MAX_PLAIN_TEXT_LENGTH
-                end,
+encode_handshake(Frag,ConnectionStates) ->
+    MaxLength = maps:get(max_fragment_length, ConnectionStates, ?MAX_PLAIN_TEXT_LENGTH),
     case iolist_size(Frag) of
 	N  when N > MaxLength ->
 	    Data = tls_record:split_iovec(erlang:iolist_to_iovec(Frag), MaxLength),
@@ -79,14 +72,8 @@ encode_alert_record(#alert{level = Level, description = Description},
 %%
 %% Description: Encodes data to send on the ssl-socket.
 %%--------------------------------------------------------------------
-encode_data(Frag, #{current_write :=
-                        #{max_fragment_length := MaxFragmentLength}} =
-                     ConnectionStates) ->
-    MaxLength = if is_integer(MaxFragmentLength) ->
-                        MaxFragmentLength;
-                   true ->
-                        ?MAX_PLAIN_TEXT_LENGTH
-                end,
+encode_data(Frag, ConnectionStates) ->
+    MaxLength = maps:get(max_fragment_length, ConnectionStates, ?MAX_PLAIN_TEXT_LENGTH),
     Data = tls_record:split_iovec(Frag, MaxLength),
     encode_iolist(?APPLICATION_DATA, Data, ConnectionStates).
 

commit 1f899c607e8d5de6d3a960b642c25e75211157eb

ssl: Correct connection state handling in TLS sender
Optimization commit 28f7e80233718f9f42fe8863370f7e99ee14d22c
broke max_fragment_length handling, that is the TLS sender
lost its knowledge of the maximum fragment length.

Make TLS sender process aware of it in the cases it is negotiated,
for default maximum we do not need to store it in the connection state.

Closes #10191

lib/ssl/src/tls_gen_connection.erl
@@ -205,8 +205,9 @@ queue_change_cipher(Msg, #state{connection_env = #connection_env{negotiated_vers
 
 reinit(#state{protocol_specific = #{sender := Sender},
               connection_env = #connection_env{negotiated_version = Version},
-              connection_states = #{current_write := Write}} = State0) ->
-    tls_sender:update_connection_state(Sender, Write, Version),
+              connection_states = #{current_write := Write} = ConnectionStates} = State0) ->
+    MaxFragLength = maps:get(max_fragment_length, ConnectionStates, undefined),
+    tls_sender:update_connection_state(Sender, Write, Version, MaxFragLength),
     State = reinit_handshake_data(State0),
     garbage_collect(),
     State.

lib/ssl/src/tls_sender.erl
@@ -41,7 +41,7 @@
          renegotiate/1,
          peer_renegotiate/1,
          downgrade/2,
-         update_connection_state/3,
+         update_connection_state/4,
          dist_handshake_complete/3]).
 
 %% gen_statem callbacks
@@ -170,12 +170,16 @@ peer_renegotiate(Pid) ->
      gen_statem:call(Pid, renegotiate, ?DEFAULT_TIMEOUT).
 
 %%--------------------------------------------------------------------
--spec update_connection_state(pid(), WriteState::map(), tls_record:tls_version()) -> ok. 
+-spec update_connection_state(pid(), WriteState::map(),
+                              tls_record:tls_version(),
+                              MaxFragLen :: tls_record:tls_max_frag_len()) -> ok.
 %% Description: So TLS connection process can synchronize the 
 %% encryption state to be used when sending application data. 
 %%--------------------------------------------------------------------
-update_connection_state(Pid, NewState, Version) ->
-    gen_statem:cast(Pid, {new_write, NewState, Version}).
+update_connection_state(Pid, NewState, Version, undefined) ->
+    gen_statem:cast(Pid, {new_write, NewState, Version});
+update_connection_state(Pid, NewState, Version, MaxFragLen) ->
+    gen_statem:cast(Pid, {new_write, NewState, Version, MaxFragLen}).
 
 %%--------------------------------------------------------------------
 -spec downgrade(pid(), integer()) -> {ok, ssl_record:connection_state()}
@@ -341,11 +345,21 @@ connection(cast, #alert{} = Alert,  #data{buff = Buff} = StateData0) ->
      end;
 connection(cast, {new_write, WritesState, Version},
            #data{connection_states = ConnectionStates, env = Env} = StateData) ->
-    CW = maps:remove(aead_handle, WritesState),
     hibernate_after(connection,
-                    StateData#data{connection_states = ConnectionStates#{current_write => CW},
-                                   env = Env#env{negotiated_version = Version}}, []);
-%%
+                    StateData#data{connection_states =
+                                       maps:without([max_fragment_length],
+                                                    ConnectionStates#{current_write => 
+                                                                          maps:remove(aead_handle, WritesState)}),
+                                   env =
+                                       Env#env{negotiated_version = Version}}, []);
+connection(cast, {new_write, WritesState, Version, MaxFragLen},
+           #data{connection_states = ConnectionStates, env = Env} = StateData) ->
+    hibernate_after(connection,
+                    StateData#data{connection_states =
+                                       ConnectionStates#{max_fragment_length => MaxFragLen,
+                                                         current_write => maps:remove(aead_handle, WritesState)},
+                                   env =
+                                       Env#env{negotiated_version = Version}}, []);
 connection(info, dist_data,
            #data{env = #env{dist_handle = DHandle}} = StateData) ->
       case dist_data(DHandle) of
@@ -427,7 +441,26 @@ handshake(cast, {new_write, WriteState0, Version},
     {next_state, connection,
      StateData#data{connection_states = ConnectionStates,
                     env = Env#env{negotiated_version = Version,
-                                           key_update_at = KeyUpdateAt}}};
+                                  key_update_at = KeyUpdateAt}}};
+handshake(cast, {new_write, WriteState, Version, MaxFragLen},
+          #data{connection_states = ConnectionStates0,
+                env = #env{key_update_at = KeyUpdateAt0,
+                           role = Role,
+                           num_key_updates = N,
+                           keylog_fun = Fun} = Env} = StateData) ->
+    ConnectionStates = ConnectionStates0#{current_write => WriteState,
+                                          max_fragment_length => MaxFragLen},
+    KeyUpdateAt = key_update_at(Version, WriteState, KeyUpdateAt0),
+    case Version of
+        ?TLS_1_3 ->
+            maybe_traffic_keylog_1_3(Fun, Role, ConnectionStates, N);
+        _ ->
+            ok
+    end,
+    {next_state, connection,
+     StateData#data{connection_states = ConnectionStates,
+                    env = Env#env{negotiated_version = Version,
+                                  key_update_at = KeyUpdateAt}}};
 handshake(info, dist_data, _) ->
     {keep_state_and_data, [postpone]};
 handshake(info, tick, _) ->